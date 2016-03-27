6d25b127a4a3ee764fe816b2ccd3ec28e007ea7d
Tom Wayson <tom@tomwayson.com>
2016-03-26 21:19:18 -0700

    exclued deps (esri, dojo) from coverage reporting
    modified template for intern config to exclude esri, dojo, etc
    no need to see the code coverage of dependencies
    this speeds up test runs significantly

--- generators/app/templates/tests/intern.js
+++ generators/app/templates/tests/intern.js
@@ -96,5 +96,5 @@ define({
 	functionalSuites: [ /* 'myPackage/tests/functional' */],
 
 	// A regular expression matching URLs to files that should not be included in code coverage analysis
-	excludeInstrumentation: /^(?:tests|node_modules)\//
+	excludeInstrumentation: /^(?:tests|node_modules|dist\/dijit|dist\/dojo|dist\/dojox|dist\/esri|dist\/moment)\//
 });
